39078abc5de6b6a1d64435f8fbf24410d72e16c6
GiraMorales <gira.morales@gamil.com>
2024-09-06 12:39:11 +0200

    root

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,3 +1,8 @@
 import './footer.css';
 
-export const footer = () => {};
+export const footer = (divContenedor) => {
+  const footer = document.createElement('footer');
+  footer.innerHTML = `<h3>By Gira Morales Revelles</h3>`;
+
+  divContenedor.append(footer);
+};

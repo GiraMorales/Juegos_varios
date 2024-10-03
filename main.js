dc282b8e718c58ac621e75fb7dcc44d2d3226969
GiraMorales <gira.morales@gamil.com>
2024-10-03 11:24:03 +0200

    dom

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 import './style.css';
 import { header } from './src/components/header/header';
 import { footer } from './src/components/footer/footer';
-
 // import './Componentes/games/theOrangeStar/init.js';
 
-//crear el espacio donde irá el juego
-const divContenedor = document.querySelector('#contenedor');
+document.addEventListener('DOMContentLoaded', () => {
+  //crear el espacio donde irá el juego
+  const divContenedor = document.querySelector('#contenedor');
 
-header(divContenedor);
+  header(divContenedor);
 
-const divContent = document.createElement('div');
-const portada = document.createElement('img');
-portada.src =
-  'https://res.cloudinary.com/dvoady6dt/image/upload/v1725898643/assets/nkuxyn2yrixprzoxqfv2.png';
+  const divContent = document.createElement('div');
+  const portada = document.createElement('img');
+  portada.src =
+    'https://res.cloudinary.com/dvoady6dt/image/upload/v1725898643/assets/nkuxyn2yrixprzoxqfv2.png';
 
-divContent.className = 'content';
-portada.className = 'portada';
+  divContent.className = 'content';
+  portada.className = 'portada';
 
-divContenedor.append(divContent);
-divContent.append(portada);
+  divContenedor.append(divContent);
+  divContent.append(portada);
 
-footer(divContenedor);
+  footer(divContenedor);
+});
